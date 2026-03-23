97ac3b4ab0ddf39d2cb5790c2827d8f66a032948
Makise0721 <ly2991374159@outlook.com>
2026-03-23 15:13:30 +0800

    feat:核心功能

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -29,6 +29,7 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/usr/share/cmake-3.28/Modules/Compiler/GNU.cmake"
   "/usr/share/cmake-3.28/Modules/FindPackageHandleStandardArgs.cmake"
   "/usr/share/cmake-3.28/Modules/FindPackageMessage.cmake"
+  "/usr/share/cmake-3.28/Modules/FindPkgConfig.cmake"
   "/usr/share/cmake-3.28/Modules/FindThreads.cmake"
   "/usr/share/cmake-3.28/Modules/Internal/CheckSourceCompiles.cmake"
   "/usr/share/cmake-3.28/Modules/Platform/Linux-GNU-C.cmake"
@@ -55,8 +56,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/ChatServer.dir/DependInfo.cmake"
   "CMakeFiles/ChatClient.dir/DependInfo.cmake"
-  "CMakeFiles/TestServer.dir/DependInfo.cmake"
-  "CMakeFiles/SimpleMymuduoServer.dir/DependInfo.cmake"
-  "CMakeFiles/DebugServer.dir/DependInfo.cmake"
   "mymuduo/CMakeFiles/mymuduo.dir/DependInfo.cmake"
   )
